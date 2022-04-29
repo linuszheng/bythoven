3c5eb237408dd1426611cdfe0ea8f002986b52ab
Linus Zheng <linus.zheng@warriorlife.net>
2022-04-29 14:28:04 -0500

    simple switch and 0 and 1

--- processor/test.v
+++ processor/test.v
@@ -1,4 +1,6 @@
 input [9:0] SW; //Input Declarations: 10 slide switches
 output [9:0] LEDR; //Output Declarations: 10 red LED lights
 
-assign LEDR = SW;
+assign LEDR[7:0] = SW[7:0];
+assign LEDR[8] = 1;
+assign LEDR[9] = 0;
