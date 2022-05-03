545e05087e8b6b974eb50c662d9084cdacd37951
mhwen <markhwen.1@gmail.com>
2022-05-02 23:34:43 -0500

    added .sdc file, slight changes to cpu.v

--- processor/cpu.v
+++ processor/cpu.v
@@ -19,38 +19,10 @@ module cpu (
     output wire [7:0] LED_G
 );
 
-    // reg [31:0] cnt = 0;
-    // reg freq = 0;
-    // reg freq2 = 0;
-    // reg [31:0] LEDcnt = 0;
-    // reg [31:0] LEDcnt2 = 0;
-    // reg LEDfreq = 1;
-    // reg LEDfreq2 = 1;
 
-    // always @(posedge clk) begin
-    //     if(cnt == 113636)
-    //         cnt <= 0;
-    //     else
-    //         cnt <= cnt+1;
-            
-    //     if(cnt2 == 160706)
-    //         cnt2 <= 0;
-    //     else
-    //         cnt2 <= cnt2+1;
-    // end
-
-    // assign f1 = cnt > 113636/128*127;
-    // assign f2 = cnt > 113636/4*3;
-
-    // assign speaker = f1;
-    // assign speaker2 = f2;
-    // assign speakerBoth = f1 | f2;
-    // assign LED = LEDfreq;
-    // assign LED2 = LEDfreq2;
-
-
-
-
+    reg [15:0] curIns;
+	 reg [17:0] sram_addr_reg;
+    reg [31:0] cycleCounter = 0;
 
 
     // calculate freq from note
@@ -58,15 +30,11 @@ module cpu (
     wire [3:0] note = curIns[3:0];
     freqCalc fc (note, 0, freq);
 
-
     // calculate cycles
     wire [63:0] bpm = 96;
     wire [63:0] cyclesPerBeat = 60 * 50000000 / bpm;
 
-    reg [15:0] curIns;
-    reg [17:0] sram_addr_reg;
-    reg [31:0] cycleCounter = 0;
-
+	//debug LED
     assign LED_R[0] = curIns[15];
 
 
@@ -76,7 +44,7 @@ module cpu (
     assign SPEAKER = freqCounter > freqCur/2;
 
     always @(posedge CLK) begin
-        if(freqCounter == freqCur) begin
+        if(freqCounter >= freqCur) begin
             freqCounter <= 0;
         end else begin
             freqCounter <= freqCounter+1;
