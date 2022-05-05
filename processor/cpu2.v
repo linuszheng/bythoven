9213f9f52c0eb318b8850ea188610ebccfb2a806
Linus Zheng <linus.zheng@warriorlife.net>
2022-05-04 22:53:33 -0500

    test harness

--- processor/cpu2.v
+++ processor/cpu2.v
@@ -5,7 +5,7 @@
 `define _CYCLES_PER_SEC 50000000
 `define _SEC_PER_MIN 60
 `define _DEFAULT_BPM 96
-`define _PLACEHOLDER_INS 16'b1000000000001111
+`define _PLACEHOLDER_INS 16'b1000000000000001
 
 module cpu2 (
     // clock
@@ -33,49 +33,48 @@ module cpu2 (
     assign SRAM_OE = 0;
     assign SRAM_LB = 0;
     assign SRAM_UB = 0;
-    assign SRAM_A = sramAddrReg;
+    assign SRAM_A  = sramAddrReg;
 
     reg sramAddrReg = 0;
 
 
 
-    // FW
-    reg [17:0] FW_pc = 0;
-    reg [15:0] FW_ins = 0;
-    wire FW_shouldFetchIns  = FW_insIsValid && !FW_insIsEnd && (!FW_insIsNote || X_cycleCounterForNotes == 0);
-    wire FW_shouldDecodeIns = FW_insIsValid && !FW_insIsEnd && (!FW_insIsNote || X_cycleCounterForNotes == 2);
+    // FW = Fetch + Wait (Multicycle)
+    reg [17:0] FR_pc = 0;
+    reg [15:0] FR_lastReadIns = `_PLACEHOLDER_INS;
+    wire FR_shouldFetchIns = FR_insIsValid && !FR_insIsEnd && (!FR_insIsNote || X_cycleCounterForNotes == 0);
+    wire FR_shouldReadIns  = FR_insIsValid && !FR_insIsEnd && (!FR_insIsNote || X_cycleCounterForNotes == 2);
 
     // Decoding
-    wire FW_insIsNote = (FW_ins[15] == 1);
-    wire FW_insIsEnd = (FW_ins[15:12] == 4'b0000);
-    wire FW_insIsBpm = (FW_ins[15:12] == 4'b0001);
-    wire FW_insIsValid = FW_insIsNote || FW_insIsBpm || FW_insIsEnd;
+    wire FR_insIsNote  = (FR_lastReadIns[15] == 1);
+    wire FR_insIsEnd   = (FR_lastReadIns[15:12] == 4'b0000);
+    wire FR_insIsBpm   = (FR_lastReadIns[15:12] == 4'b0001);
+    wire FR_insIsValid = FR_insIsNote || FR_insIsBpm || FR_insIsEnd;
 
     // Music Properties
-    wire FW_insBpm = FW_ins[11:0];
+    wire FR_insBpm = FR_lastReadIns[11:0];
 
     // Initial settings
-    reg FW_bpm = `_DEFAULT_BPM;
+    reg FR_bpm = `_DEFAULT_BPM;
 
     // -----------------------------[ STAGE: FETCH    ]------------------------------
-    wire FW_fetch = X_cycleCounterForNotes % 3 == 0;
+    wire FR_fetch = X_cycleCounterForNotes % 3 == 0;
     always @(posedge CLK) begin
-        if(FW_fetch && FW_shouldFetchIns) begin
-            sramAddrReg <= FW_pc;
+        if(FR_fetch && FR_shouldFetchIns) begin
+            sramAddrReg <= FR_pc;
         end
     end
 
 
-
-    // -----------------------------[ STAGE:  WAIT    ]-------------------------------
-    wire FW_wait = X_cycleCounterForNotes % 3 == 2;
+    // -----------------------------[ STAGE:  READ    ]-------------------------------
+    wire FR_read = X_cycleCounterForNotes % 3 == 2;
     always @(posedge CLK) begin
-        if(FW_wait && FW_shouldDecodeIns) begin
-            FW_ins <= SRAM_D;
-            FW_pc <= FW_pc+1;
+        if(FR_read && FR_shouldReadIns) begin
+            FR_lastReadIns <= SRAM_D;
+            FR_pc <= FR_pc+1;
 
-            if(FW_insIsBpm) begin
-                FW_bpm <= FW_insBpm;
+            if(FR_insIsBpm) begin
+                FR_bpm <= FR_insBpm;
             end
         end
     end
@@ -84,34 +83,31 @@ module cpu2 (
 
     // -----------------------------[ STAGE:  EXECUTE ]-------------------------------
     reg X_insIsValid = 1;
-    reg [31:0] X_cycleCounter = 0;
     reg [15:0] X_ins = `_PLACEHOLDER_INS;
 
     reg [31:0] X_cycleCounterForNotes = 0;
     reg [31:0] X_cycleCounterForSoundWaves = 0;
 
     // Music Properties
-    reg [31:0] X_bpm = 1;
+    reg [31:0] X_bpm = `_DEFAULT_BPM;
 
     // Note Decoding
     wire X_insIsNote = (X_ins[15] == 1);
     wire [3:0] X_note = X_ins[3:0];
     wire [1:0] X_octave = X_ins[5:4];
-    wire [2:0] X_volume = X_ins[7:6]+1;
+    wire [2:0] X_volume = X_ins[7:6];
     wire [3:0] X_lengthCode = X_ins[11:8];
     wire [1:0] X_styleCode = X_ins[13:12];
     wire X_extraInfo = X_ins[14];
 
     // note
     wire [63:0] X_cyclesPerBeat = `_CYCLES_PER_SEC * `_SEC_PER_MIN / X_bpm;
-    wire [63:0] X_cyclesPerNote;
-    lengthCalc lc (X_lengthCode, X_cyclesPerBeat, X_cyclesPerNote);
+    wire [63:0] X_cyclesPerNote; lengthCalc lc (X_lengthCode, X_cyclesPerBeat, X_cyclesPerNote);
 
     // soundwave
     wire [31:0] X_soundWavesPerSec;     // = X_freq
     wire [31:0] X_cyclesPerSoundWave = `_CYCLES_PER_SEC / X_soundWavesPerSec;
-    wire X_freqIsValid;
-    freqCalc fc (X_note, X_octave, X_soundWavesPerSec, X_freqIsValid);
+    wire X_freqIsValid; freqCalc fc (X_note, X_octave, X_soundWavesPerSec, X_freqIsValid);
 
     // speaker
     wire X_separationPause = X_cycleCounterForSoundWaves > X_cyclesPerSoundWave - 2000000;
@@ -126,10 +122,10 @@ module cpu2 (
 
     // transfer information from FW to X
     always @(posedge CLK) begin
-        if(X_cycleCounter == 0) begin
-            X_bpm <= FW_bpm;
-            X_ins <= FW_ins;
-            X_insIsValid <= FW_insIsValid;
+        if(X_cycleCounterForNotes == 0) begin
+            X_bpm <= FR_bpm;
+            X_ins <= FR_lastReadIns;
+            X_insIsValid <= FR_insIsValid;
         end
     end
 
